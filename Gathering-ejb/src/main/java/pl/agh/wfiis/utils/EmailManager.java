dae5109ab4dcdc0209290d4be245ee5d677eaacc
Patryk <patryk.lesiakk@gmail.com>
2016-01-05 22:19:09 +0100

    Robocze wysyłanie maila

--- Gathering-ejb/src/main/java/pl/agh/wfiis/utils/EmailManager.java
+++ Gathering-ejb/src/main/java/pl/agh/wfiis/utils/EmailManager.java
@@ -1,64 +1,63 @@
 package pl.agh.wfiis.utils;
 
-import java.util.Date;
 import java.util.Properties;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 import javax.ejb.Stateless;
 import javax.ejb.LocalBean;
-import javax.mail.Authenticator;
-import javax.mail.Message;
-import javax.mail.MessagingException;
-import javax.mail.NoSuchProviderException;
-import javax.mail.PasswordAuthentication;
-import javax.mail.Session;
-import javax.mail.Transport;
+import javax.mail.*;
 import javax.mail.internet.InternetAddress;
 import javax.mail.internet.MimeMessage;
 
-    class Autentyfikator extends Authenticator
-    {
-        @Override
-        public PasswordAuthentication getPasswordAuthentication()
-        {
-            return new PasswordAuthentication("projekt.io","wfiisagh");
-        }
+class Autentyfikator extends Authenticator {
+    @Override
+    public PasswordAuthentication getPasswordAuthentication() {
+        return new PasswordAuthentication("projekt.io", "wfiisagh");
     }
+}
 
 @Stateless
 @LocalBean
 public class EmailManager {
-Logger logger = Logger.getLogger(getClass().getName());
+
     public void sendEmail() {
-    Properties props = new Properties();
-    props.setProperty("mail.transport.protocol", "smtp");
-    props.setProperty("mail.host", "poczta.o2.pl");
-    props.setProperty("mail.user", "projekt.io");
-    props.setProperty("mail.password", "wfiisagh");
-    props.setProperty("mail.smtp.auth", "true");
-    props.setProperty("mail.from", "projekt.io@o2.pl");
-    
-    Session session = Session.getInstance(props, new Autentyfikator());
-    
-      Transport transport;
+        Properties props = prepareEmailProperties();
+        Session session = Session.getInstance(props, new Autentyfikator());
+
         try {
-            transport = session.getTransport();
-
-            MimeMessage message = new MimeMessage(session);
-            message.setSubject("Testing javamail plain");
-            message.setContent("This is a test", "text/plain");
-            message.addRecipient(Message.RecipientType.TO, new InternetAddress("patryk.lesiak@o2.pl"));
-
-            transport.connect();
-            transport.sendMessage(message, message.getRecipients(Message.RecipientType.TO));
-            transport.close();
-            logger.info("OK");
-            
-        } catch (NoSuchProviderException ex) {
-            Logger.getLogger(EmailManager.class.getName()).log(Level.SEVERE, null, ex);
-        } catch (MessagingException ex) {
+            sendMessage(createMessage(session), session);
+        } catch (Exception ex) {
             Logger.getLogger(EmailManager.class.getName()).log(Level.SEVERE, null, ex);
         }
-        
+    }
+
+    private Properties prepareEmailProperties() {
+        Properties properties = new Properties();
+        properties.setProperty("mail.transport.protocol", "smtp");
+        properties.setProperty("mail.host", "poczta.o2.pl");
+        properties.setProperty("mail.user", "projekt.io");
+        properties.setProperty("mail.password", "wfiisagh");
+        properties.setProperty("mail.smtp.auth", "true");
+        properties.setProperty("mail.from", "projekt.io@o2.pl");
+
+        return properties;
+    }
+
+    private MimeMessage createMessage(Session session) throws MessagingException {
+        MimeMessage message = new MimeMessage(session);
+        message.setSubject("Gathering - potwierdzenie zapisu");
+        message.setContent("This is a test", "text/plain");
+        message.setFrom("projekt.io@o2.pl");
+        message.addRecipient(Message.RecipientType.TO, new InternetAddress("patryk.lesiakk@gmail.com"));
+
+        return message;
+    }
+
+    private void sendMessage(MimeMessage message, Session session) throws NoSuchProviderException, MessagingException {
+        Transport transport;
+        transport = session.getTransport();
+        transport.connect();
+        transport.sendMessage(message, message.getRecipients(Message.RecipientType.TO));
+        transport.close();
     }
 }
